e1177f6d30eeefce41f8720953e9f3423c3ab326
JenniferG-Parrales <85895809+JenniferG-Parrales@users.noreply.github.com>
2021-12-02 10:25:10 -0600

    Add files via upload

---
+++ ExamenProg-master/Presentacion/BuscarEstuFrm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class BuscarEstuFrm : Form
+    {
+        public BuscarEstuFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegresaralMenu_Click(object sender, EventArgs e)
+        {
+            MenuFrm RMenu = new MenuFrm();
+            RMenu.Show();
+            this.Hide();
+        }
+    }
+}

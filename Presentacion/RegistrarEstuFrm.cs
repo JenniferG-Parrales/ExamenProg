ee046607bdbe06c974f21f4a6dc164d69f7f971f
JenniferG-Parrales <85895809+JenniferG-Parrales@users.noreply.github.com>
2021-12-02 10:27:01 -0600

    Add files via upload

---
+++ Presentacion/RegistrarEstuFrm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class RegistrarEstuFrm : Form
+    {
+        public RegistrarEstuFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegresaralMenu_Click(object sender, EventArgs e)
+        {
+            MenuFrm RMenu = new MenuFrm();
+            RMenu.Show();
+            this.Hide();
+        }
+    }
+}
